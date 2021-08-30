0db39f30c3505c641ce0d6c948f5bb2d88f042f2
Cfon <kimalekkz@gmail.com>
2021-08-31 00:36:24 +0500

    Updated index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-$connection = mysqli_connect('sql3.freemysqlhosting.net', 'sql3433617', 'eM9lT7XZNS', 'sql3433617');
+
+echo 'setWebhook'.PHP_EOL;
+
+/* $connection = mysqli_connect('sql3.freemysqlhosting.net', 'sql3433617', 'eM9lT7XZNS', 'sql3433617');
 
 if (!$connection) {
 	exit('Невозможно подключиться к базе данных: <br>') . mysql_error();
@@ -17,9 +20,9 @@ if (!$result) {
 	exit('Невозможно выполнить запрос к базе данных: <br>') . mysql_error();
 }
 
-// var_dump($result);
-?>
-<!doctype html>
+// var_dump($result); */
+
+/* <!doctype html>
 <html lang="en">
   <head>    
     <meta charset="utf-8">
@@ -36,7 +39,7 @@ if (!$result) {
 			  <button class="btn btn-success mt-2">
 			    <i class="fa fa-plus"></i>					
 			  </button>
-			  <table class="table">
+			  <table class="table table-primary">
 				  <thead>
 					<tr>
 					  <th scope="col">#</th>
@@ -77,4 +80,4 @@ if (!$result) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
     -->
   </body>
-</html>
+</html> */
