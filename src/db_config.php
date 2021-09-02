0eec08b1fbf2ece802b86542489574907120a532
Cfon <kimalekkz@gmail.com>
2021-09-03 00:41:08 +0500

    Updated files

--- src/db_config.php
+++ src/db_config.php
@@ -2,16 +2,15 @@
 namespace App\HerokuDemoApp;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+	
+if (file_exists(__DIR__ .'/../.env')) {	
+	$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+	$dotenv->load();	
+} 
 
-// $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
-// $dotenv->load();
+define('HOST', $_ENV['MYSQL_HOST']);
+define('DB_NAME', $_ENV['DB_NAME']);
+define('USERNAME', $_ENV['USER_NAME']);
+define('PASSWORD', $_ENV['DB_PASSWORD']);
 
-// define('HOST', $_ENV['MYSQL_HOST']);
-// define('DB_NAME', $_ENV['DB_NAME']);
-// define('USERNAME', $_ENV['USER_NAME']);
-// define('PASSWORD', $_ENV['DB_PASSWORD']);
 
-define('HOST', 'sql3.freemysqlhosting.net');
-define('DB_NAME', 'sql3433617');
-define('USERNAME', 'sql3433617');
-define('PASSWORD', 'eM9lT7XZNS');
